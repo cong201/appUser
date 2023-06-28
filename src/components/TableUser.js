1d33311ce4266843e59755e64a9b4173b9ef822e
Công Lê <levancong_t64@gmail.com>
2023-06-28 17:47:00 +0700

    phân trang , nhưng chưa được , nhìn hơi đần

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react"
 import axios from 'axios';
 import { fetchAllUser } from "../service/UserService";
-
+import ReactPaginate from "react-paginate";
 const TableUser = (props) => {
 
     const [listUsers, setListUsers] = useState([])
+    const [totalUsers, setTotalUsers] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
 
     useEffect(() => {
-        getUser()
+        getUser(1)
     }, [])
 
-    const getUser = async () => {
-        let res = await fetchAllUser();
-        if (res && res.data && res.data.data) {
-            setListUsers(res.data.data)
+    const getUser = async (page) => {
+        let res = await fetchAllUser(page);
+        if (res && res.data) {
+            setTotalUsers(res.total)
+            setListUsers(res.data)
+            setTotalPages(res.total_pages)
         }
-
     }
-    console.log(listUsers)
+
+    const handlePageClick = (event) => {
+        console.log("hello", event);
+        getUser(+event.selected + 1)
+    }
     return (
         <div className="flex flex-col container mx-10 mt-20">
             <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
@@ -55,6 +62,26 @@ const TableUser = (props) => {
                                 }
                             </tbody>
                         </table>
+                        <ReactPaginate
+                            nextLabel="next >"
+                            onPageChange={handlePageClick}
+                            pageRangeDisplayed={3}
+                            marginPagesDisplayed={2}
+                            pageCount={totalPages}
+                            previousLabel="< previous"
+                            pageClassName="page-item"
+                            pageLinkClassName="page-link"
+                            previousClassName="page-item"
+                            previousLinkClassName="page-link"
+                            nextClassName="page-item"
+                            nextLinkClassName="page-link"
+                            breakLabel="..."
+                            breakClassName="page-item"
+                            breakLinkClassName="page-link"
+                            containerClassName="pagination"
+                            activeClassName="active"
+                            renderOnZeroPageCount={null}
+                        />
                     </div>
                 </div>
             </div>
