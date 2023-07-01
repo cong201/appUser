76475f6d9cb8b55fa77af1e617fffbb875d710f4
Công Lê <levancong_t64@gmail.com>
2023-07-01 16:17:19 +0700

    update phan edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
-<<<<<<< HEAD
-=======
 
->>>>>>> 5aeae959225dc610e552681506c59790d12f8902
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> 5aeae959225dc610e552681506c59790d12f8902
+
+
